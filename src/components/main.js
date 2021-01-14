696531376a53899398d8de61e3426a64a38bfe51
Alex Miller <asmiller1989@gmail.com>
2021-01-14 11:54:55 -0600

    Handles the network URL param to find a type of Lattice We were previously depending on the routing mechanism coming from the Lattice keyring module, but we ran into an issue which revealed the brittleness of that. For external connections, we only want to pop up the connection page anyway, after which point the window closes. We should be doing the routing on this site rather than expecting external tools to know how to route to development vs production Lattices.

--- src/components/main.js
+++ src/components/main.js
@@ -89,7 +89,7 @@ class Main extends React.Component {
         const prevKeyringLogin = this.getPrevKeyringLogin();
         const keyringTimeoutBoundary = new Date().getTime() - constants.KEYRING_LOGOUT_MS;
         if (prevKeyringLogin && prevKeyringLogin.lastLogin > keyringTimeoutBoundary) {
-          this.connect(prevKeyringLogin.deviceID, prevKeyringLogin.password, () => this.connectSession(prevKeyringLogin))
+          this.startSDKSession(prevKeyringLogin.deviceID, prevKeyringLogin.password, () => this.connectSession(prevKeyringLogin))
         } else {
           // If the login has expired, clear it now.
           this.clearPrevKeyringLogin();
@@ -100,7 +100,7 @@ class Main extends React.Component {
       const deviceID = window.localStorage.getItem('gridplus_web_wallet_id');
       const password = window.localStorage.getItem('gridplus_web_wallet_password');
       if (deviceID && password)
-        this.connect(deviceID, password, () => this.connectSession())
+        this.startSDKSession(deviceID, password, () => this.connectSession())
     }
   }
 
@@ -121,11 +121,15 @@ class Main extends React.Component {
     return this.state.windowWidth < 500;
   }
 
-  connect(deviceID, password, cb) {
+  startSDKSession(deviceID, password, cb) {
+    // We want to pay attention to the URL network parameter, as it ultimately determines
+    // how we try to contact the Lattice.
+    const params = new URLSearchParams(window.location.search);
+    const network = params.get('network');
     const updates = { deviceID, password };
     if (!this.state.session) {
       // Create a new session if we don't have one.
-      updates.session = new SDKSession(deviceID, this.handleStateUpdate, this.state.name);
+      updates.session = new SDKSession(deviceID, this.handleStateUpdate, this.state.name, network);
     }
     this.setState(updates, cb);
   }
@@ -334,7 +338,7 @@ class Main extends React.Component {
       this.setError(null);
     }
     // Connect to the device
-    this.connect(deviceID, password, () => {
+    this.startSDKSession(deviceID, password, () => {
       // Create a new session with the deviceID and password provided.
       if (showLoading === true)
         this.wait("Looking for your Lattice");
