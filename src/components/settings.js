988f4f6da8be637cc8ef8abf928df045ea8641c1
Alex Miller <asmiller1989@gmail.com>
2021-02-04 11:41:12 -0600

    Updates keyring storage mechanism and adds management setting for 3rd party logins

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'antd/dist/antd.css'
-import { Button, Card, Col, Icon, Input, Row, Switch } from 'antd'
+import { Button, Card, Col, Collapse, Icon, Input, Row, Switch, Table } from 'antd'
 import './styles.css'
 import { constants, } from '../util/helpers';
 const settingsPath = `${constants.ROOT_STORE}/settings`
@@ -10,8 +10,11 @@ class Settings extends React.Component {
     super(props);
 
     this.state = {
-      settings: {},
-      local: {}
+      settings: {
+        customEndpoint: '',
+        keyringLogins: {},
+      },
+      local: {},
     }
 
     this.getSettings();
@@ -53,36 +56,81 @@ class Settings extends React.Component {
     this.setState({ settings });
   }
 
-  renderCard() {
+  removeKeyring(login) {
+    const settings = this.state.settings || {};
+    delete settings.keyringLogins[login.name]
+    this.setState({ settings })
+  }
+
+  renderCustomEndpointSetting() {
     const { customEndpoint='' } = this.state.settings;
     let { useCustomEndpoint=false } = this.state.local;
     if (customEndpoint !== '')
       useCustomEndpoint = true;
     return (
+      <Card>
+        <h3>(Advanced) Connection Endpoint:</h3>
+        <p><i>
+          If you wish to route messages and connections through your own endpoint, you may specify it here. 
+          See <a href="https://github.com/GridPlus/lattice-connect" target="_blank">this</a> for more information.
+        </i></p>
+        <p><b>Use Custom:</b></p>
+        <Switch checked={useCustomEndpoint} onChange={this.updateUseCustomEndpoint.bind(this)}/>
+        {useCustomEndpoint === true ? (
+          <div>
+            <br/>
+            <p><b>Custom Endpoint:</b></p>
+            <Input  placeholder="host:port" 
+                    defaultValue={customEndpoint} 
+                    onChange={this.updateCustomEndpoint.bind(this)}/>
+          </div>
+        ) : null}
+        <br/>
+      </Card>
+    )
+  }
+
+  renderKeyringsSetting() {
+    const { keyringLogins = {} } = this.state.settings;
+    const cols = [
+      { 
+        title: 'App Name', 
+        dataIndex: 'name', 
+        key: 'name'
+      }, 
+      { 
+        title: 'Action', 
+        dataIndex: 'action', 
+        key: 'action',
+        render: (text, record) => (
+          <Button type="link" onClick={() => {this.removeKeyring(record)}}>Forget</Button>
+        ) 
+      }
+    ]
+    const data = [];
+    Object.keys(keyringLogins)
+      .sort((a, b) => { return a.toLowerCase() > b.toLowerCase() ? 1 : -1 })
+      .forEach((name) => { data.push({ name }) })
+    return (
+      <Card>
+        <h3>Third Party Connections</h3>
+        <p><i>
+          You may forget a third party here and next time you use the app you will be prompted to create a new one.
+        </i></p>
+        <Collapse>
+          <Collapse.Panel header={`Connections List (${data.length})`}>
+            <Table dataSource={data} columns={cols}/>
+          </Collapse.Panel>
+        </Collapse>
+      </Card>
+    )
+  }
+
+  renderCard() {
+    return (
       <div>
-        <Card>
-          <h3>(Advanced) Connection Endpoint:</h3>
-          <p><i>
-            If you wish to route messages and connections through your own endpoint, you may specify it here. 
-            See <a href="https://github.com/GridPlus/lattice-connect" target="_blank">this</a> for more information.
-          </i></p>
-          <Row>
-            <Col>
-              <p><b>Use Custom:</b></p>
-              <Switch checked={useCustomEndpoint} onChange={this.updateUseCustomEndpoint.bind(this)}/>
-              {useCustomEndpoint === true ? (
-                <div>
-                  <br/>
-                  <p><b>Custom Endpoint:</b></p>
-                  <Input  placeholder="host:port" 
-                          defaultValue={customEndpoint} 
-                          onChange={this.updateCustomEndpoint.bind(this)}/>
-                </div>
-              ) : null}
-            </Col>
-          </Row>
-          <br/>
-        </Card>
+        {this.renderKeyringsSetting()}
+        {this.renderCustomEndpointSetting()}
         <br/>
         <Button type="primary" onClick={this.submit.bind(this)}>
           Update and Reload
