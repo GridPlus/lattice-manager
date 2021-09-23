fbac9ae0e88f32b1a560870f995e3c724d532422
Alex Miller <asmiller1989@gmail.com>
2021-09-23 15:56:38 -0500

    Updates connect page

--- src/components/connect.js
+++ src/components/connect.js
@@ -24,6 +24,12 @@ class Connect extends React.Component {
     this.setState({ isLoading: false });    
   }
 
+  componentDidUpdate() {
+    if (this.props.name !== 'GridPlus Web Wallet' && document.title !== 'Lattice Connector') {
+      document.title = 'Lattice Connector'
+    }
+  }
+
   componentWillUnmount() {
     this.setState({ isLoading: false })
   }
@@ -106,6 +112,39 @@ class Connect extends React.Component {
     return this.state.windowWidth < 500;
   }
 
+  renderSetupInfo() {
+    return (
+      <div>
+        <center>
+          <h3><b>New User Setup</b></h3>
+        </center>
+        <p>
+          You can use this page to establish a connection between <b>{this.props.name}</b>&nbsp; and your Lattice 
+          hardware wallet device.&nbsp;
+          <i>For more general device setup information, please see the 
+            <a href="https://gridplus.io/setup" target={"_blank"} rel={"noopener noreferrer"}>
+            Lattice setup page
+            </a>.</i>
+        </p>
+        <h3><b>Step 1:</b></h3>
+        <p>
+          Unlock your Lattice and find its <b>Device ID</b> on the main menu. This is a six-character code.
+        </p>
+        <h3><b>Step 2:</b></h3>
+        <p>
+          Once you have your Device ID, specify a <b>password</b>. This does <i>not</i> secure any value and 
+          is <i>not</i> associated with your wallet seed - it is only used to send secure requests to your device. 
+          If you lose your password, you can remove the permission on your device and re-connect with a new one.
+        </p>
+        <h3><b>Step 3:</b></h3>
+        <p>
+          Please ensure your Lattice is <b>online</b> and click "Connect". Your device is online if there is a single
+          wifi signal icon on the top-right of the screen.
+        </p>
+      </div>
+    )
+  }
+
   renderModal() {
     if (this.state.showSettings) {
       return (
@@ -122,44 +161,12 @@ class Connect extends React.Component {
      return (
       <div>
         <Modal
-          title="GridPlus Web Wallet"
+          title={this.props.name === 'GridPlus Web Wallet' ? this.props.name : 'Lattice1 Connector 🔗'}
           visible={this.state.modal}
           onOk={this.hideModal.bind(this)}
           onCancel={this.hideModal.bind(this)}
         >
-          <center>
-            <h3><b>New User Setup</b></h3>
-          </center>
-          <p>
-            With the GridPlus Web Wallet you can monitor balances and transactions for your Lattice1 device. The following
-            guide is meant to explain this login process.
-            <br/><br/>
-            <i>For more general setup information, please see the <a href="https://gridplus.io/setup" target={"_blank"}>
-              Lattice1 setup page
-            </a>.</i>
-          </p>
-          <h3><b>Step 1:</b></h3>
-          <p>
-            In order to connect to your device, find its <b>Device ID</b>:
-          </p>
-          <p>
-            {deviceIdContent}
-          </p>
-          <h3><b>Step 2:</b></h3>
-          <p>
-            Once you have your Device ID, specify a <b>password</b>:
-          </p>
-          <p>
-            {pwContent}
-          </p>
-          <p><i>
-            This password is not stored on any server or locally so there is no way to reset it.
-            However, if you forget your password, you can always create a new one and reconnect to your Lattice1.
-          </i></p>
-          <h3><b>Step 3:</b></h3>
-          <p>
-            <br/><br/>Please ensure your Lattice1 is <b>online</b> and click "Connect".
-          </p>
+          {this.renderSetupInfo()}
         </Modal>
       </div>
     );
@@ -203,16 +210,16 @@ class Connect extends React.Component {
               <a href="https://gridplus.io/lattice" target={"_blank"}>
                 <img alt="GridPlus" src={'/gridplus-logo-black.png'}/>
                 {keyringName ? (
-                  <h2 style={{margin: "10px 0 0 0"}}>Lattice1 Connector <Icon type="link"/></h2>
+                  <h2 style={{margin: "10px 0 0 0"}}>Lattice Connector <Icon type="link"/></h2>
                 ) : (
                   <h2 style={{margin: "10px 0 0 0"}}>Web Wallet <Icon type="wallet"/></h2>
                 )}
               </a>
-              {keyringName ? (<div><br/><i><p>Login for <b>{keyringName}</b></p></i></div>) : null}
+              {keyringName ? (<div><br/><i><h3>Connect to:</h3></i><h2>{keyringName}</h2></div>) : null}
               {tooLong ? (<p><b>Error: App name must be more than 4 characters.</b></p>) : this.renderForm()}
             </Card>
             <Button type="link" onClick={this.showModal.bind(this)} style={{margin: "20px 0 0 0"}}>
-              New User?
+              New User Info
             </Button>
             <br/>
             <Button type="link" onClick={() => {
@@ -220,9 +227,10 @@ class Connect extends React.Component {
             }}>
               Settings
             </Button>
-            <a href="https://gridplus.io/lattice" target={"_blank"}>
-              <p>Buy a Lattice1</p>
-            </a>
+            <br/>
+            <Button type="link" href="https://gridplus.io/lattice" target={"_blank"} rel={"noopener noreferrer"}>
+              About the Lattice
+            </Button>
           </center>
         </Col>
       </Row>
@@ -231,36 +239,3 @@ class Connect extends React.Component {
 }
 
 export default Connect
-
-
-const deviceIdContent = (
-  <Collapse defaultActiveKey={['1']}>
-    <Panel header="What is this?" key="1">
-      <p>The DeviceID is a unique fingerprint of your Lattice1 device. It is used to find your Lattice over the internet.</p>
-    </Panel>
-    <Panel header="How do I find my DeviceID?" key="2">
-      <p>On your Lattice1's home screen, you should see an item titled "Device ID". Click this to find the ID.</p> 
-      <p>The DeviceID contains 6 to 8 random numbers and letters.</p>
-    </Panel>
-    <Panel header="Why do I need to enter this?" key="3">
-      <p>This web wallet does not save or send any information about you or your device.</p>
-      <p>After logging out, you must re-enter your device ID in order to find your device.</p>
-    </Panel>
-  </Collapse>
-);
-
-const pwContent = (
-  <Collapse defaultActiveKey={['1']}>
-    <Panel header="What is this?" key="1">
-      <p>The password you enter is used to create a unique connection to your Lattice1 device.</p>
-    </Panel>
-    <Panel header="Where is the password stored?" key="2">
-      <p>It isn't stored at all. It never leaves this page and if you come back, it will not be saved.</p> 
-      <p>You should back it up yourself.</p>
-    </Panel>
-    <Panel header="What if I lose my password?" key="3">
-      <p>No problem! Just go to your Lattice1 device, open up your "Connections" page,<br/>and delete the connection called "GridPlus Web Wallet".</p>
-      <p>Then simply create a new password here (and save it offline) to re-connect to your Lattice.</p>
-    </Panel>
-  </Collapse>
-);
