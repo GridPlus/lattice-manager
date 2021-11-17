f3038ed3143c2dbc1037b4e7d71276879a47130d
Alex Miller <asmiller1989@gmail.com>
2021-11-17 09:44:04 -0600

    Updates pairing component

--- src/components/pair.js
+++ src/components/pair.js
@@ -2,15 +2,7 @@ import React from 'react';
 import { Card, Input } from 'antd'
 import { PageContent } from './index'
 import 'antd/dist/antd.dark.css'
-
-const INPUT_STYLE = {
-  'margin': '20px 0 0 0',
-  'textAlign': 'center',
-  'backgroundColor': '#1890ff',
-  'color': 'white',
-  'fontFamily': 'Andale Mono;Courier',
-  'letterSpacing': '10px'
-}
+const SUBMIT_LEN = 8; // 8 characters in a code
 
 class Pair extends React.Component {
   constructor(props) {
@@ -23,19 +15,16 @@ class Pair extends React.Component {
   componentDidMount() {
     this.setState({ code: '' })
     this.input.focus()
-    const height = document.getElementById("secret").offsetHeight
-    if (height > 0)
-      INPUT_STYLE['fontSize'] = 0.9 * height
   }
 
-  componentDidUnmount() {
+  componentWillUnmount() {
     this.setState({ code: '' })
   }
 
   handleUpdate(e) {
     try {
       this.setState({ code: e.target.value.toUpperCase() }, () => {
-        if (this.state.code.length > 7) {
+        if (this.state.code.length >= SUBMIT_LEN) {
           this.props.submit(this.state.code)
         }
       })
@@ -44,20 +33,36 @@ class Pair extends React.Component {
     }
   }
 
+  getBoxWidth() {
+    const e = document.getElementById('secret-card')
+    if (!e)
+      return
+    return 0.8 * e.clientWidth
+  }
+
+  getBoxFontHeight() {
+    const w = this.getBoxWidth();
+    if (!w)
+      return;
+    return 0.6 * (w / SUBMIT_LEN);
+  }
+
   render() {
     const size = this.props.isMobile() ? 'small' : 'large';
-    const searchWidth = this.props.isMobile() ? "100%" : "80%";
+    const width = this.getBoxWidth();
+    const fontSize = this.getBoxFontHeight();
     const content = (
       <center>
-        <Card title="Enter Secret" bordered={true}>
+        <Card title="Enter Secret" bordered={true} id='secret-card'>
           <p></p>
           <p>Please enter the pairing secret displayed on your Lattice screen:</p>
-          <Input 
+          <Input
+            className='lattice-pairing-box'
             size={size}
             id="secret"
             ref={i => {this.input = i}}
             onChange={this.handleUpdate.bind(this)}
-            style={{width: searchWidth, ...INPUT_STYLE}}
+            style={{width, fontSize}}
             value={this.state.code}
           />
         </Card>
