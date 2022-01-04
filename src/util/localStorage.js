f9aa06df3d7404c728b674d354e0b097e68dab0f
Doug Lance <doug.lance@gmail.com>
2022-01-04 10:09:52 -0500

    Moves localStorage constants to avoid circular dependencies

--- src/util/localStorage.js
+++ src/util/localStorage.js
@@ -1,33 +1,35 @@
-import { constants } from "./helpers";
 import omit from "lodash/omit";
+const WALLET_ID_STORAGE_KEY = "gridplus_web_wallet_id";
+const WALLET_PASSWORD_STORAGE_KEY = "gridplus_web_wallet_password";
+const ROOT_STORE = process.env.REACT_APP_ROOT_STORE || "gridplus";
 
 const getItem = (key) => JSON.parse(window.localStorage.getItem(key));
 const setItem = (key, value) =>
   window.localStorage.setItem(key, JSON.stringify(value));
 const removeItem = (key) => window.localStorage.removeItem(key);
 
-const getRootStore = () => getItem(constants.ROOT_STORE) ?? {};
+const getRootStore = () => getItem(ROOT_STORE) ?? {};
 const setRootStore = (value) =>
   window.localStorage.setItem(
-    constants.ROOT_STORE,
+    ROOT_STORE,
     JSON.stringify({ ...getRootStore(), ...value })
   );
-const removeRootStore = () => removeItem(constants.ROOT_STORE);
+const removeRootStore = () => removeItem(ROOT_STORE);
 
-const getRootStoreItem = (key) => getItem(constants.ROOT_STORE)?.[key] ?? {};
+const getRootStoreItem = (key) => getItem(ROOT_STORE)?.[key] ?? {};
 const setRootStoreItem = (key, value) =>
   window.localStorage.setItem(
-    constants.ROOT_STORE,
+    ROOT_STORE,
     JSON.stringify({ ...getRootStore(), [`${key}`]: value })
   );
 const removeRootStoreItem = (key) =>
   window.localStorage.setItem(
-    constants.ROOT_STORE,
+    ROOT_STORE,
     JSON.stringify(omit(getRootStore(), key))
   );
 
 const getSettings = () => getRootStoreItem("settings");
-const setSettings = (value) => setRootStoreItem("settings", value)
+const setSettings = (value) => setRootStoreItem("settings", value);
 
 const getKeyring = () => getRootStoreItem("keyring");
 
@@ -37,15 +39,14 @@ const setKeyringItem = (key, value) =>
 const removeKeyringItem = (key) =>
   setRootStoreItem("keyring", omit(getKeyring(), key));
 
-const getWalletId = () => getItem(constants.WALLET_ID_STORAGE_KEY);
-const setWalletId = (value) => setItem(constants.WALLET_ID_STORAGE_KEY, value);
-const removeWalletId = () => removeItem(constants.WALLET_ID_STORAGE_KEY);
+const getWalletId = () => getItem(WALLET_ID_STORAGE_KEY);
+const setWalletId = (value) => setItem(WALLET_ID_STORAGE_KEY, value);
+const removeWalletId = () => removeItem(WALLET_ID_STORAGE_KEY);
 
-const getWalletPassword = () => getItem(constants.WALLET_PASSWORD_STORAGE_KEY);
+const getWalletPassword = () => getItem(WALLET_PASSWORD_STORAGE_KEY);
 const setWalletPassword = (value) =>
-  setItem(constants.WALLET_PASSWORD_STORAGE_KEY, value);
-const removeWalletPassword = () =>
-  removeItem(constants.WALLET_PASSWORD_STORAGE_KEY);
+  setItem(WALLET_PASSWORD_STORAGE_KEY, value);
+const removeWalletPassword = () => removeItem(WALLET_PASSWORD_STORAGE_KEY);
 
 export default {
   getItem,
