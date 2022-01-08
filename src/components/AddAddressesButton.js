01cc01baae95077552a8106b0b7071253a1b3a1f
Doug Lance <doug.lance@gmail.com>
2022-01-07 20:26:34 -0500

    Lints code for readability

--- src/components/AddAddressesButton.js
+++ src/components/AddAddressesButton.js
@@ -1,5 +1,6 @@
-import { PlusOutlined } from "@ant-design/icons";
-import { Alert, Button, Input, Modal, Space } from "antd";
+import uuid from "uuid";
+import { PlusOutlined, MinusOutlined } from "@ant-design/icons";
+import { Alert, Button, Form, Input, Modal, Space } from "antd";
 import _ from "lodash";
 import React, { useEffect, useState } from "react";
 
@@ -8,66 +9,88 @@ const keyIsDuplicatedErrorMessage =
   "You already have a tag with this address on your device.";
 const valIsDuplicatedErrorMessage =
   "You already have a tag with this name on your device.";
-const getDefaultRecord = ()=>({ key: null, val: null, isKeyValid: false, isValValid: false });
 
-/**
- * @typedef {{ key: string, val: string }} Record
- */
+const getDefaultRecord = () => ({
+  key: null,
+  val: null,
+  isKeyValid: false,
+  isValValid: false,
+  id: uuid.v4(),
+});
 
-/**
- * @typedef {{ key: string, val: string, isKeyValid: boolean, isValValid: boolean }} RecordToAdd
- */
+/** @typedef {{ key: string, val: string }} Record */
+/** @typedef { Record & { isKeyValid: boolean, isValValid: boolean, id: string }} RecordToAdd */
 
-const ErrorAlert = ({ errorMessage }) => (
-  <>{errorMessage ? <Alert description={errorMessage} type="error" style={{width:"100%"}} /> : null}</>
-);
+const ErrorAlert = ({ errorMessage }) =>
+  errorMessage ? (
+    <Alert description={errorMessage} type="error" style={{ width: "100%" }} />
+  ) : null;
 
 /**
  * @name AddAddressForm
  * @param {Object} props
  * @param {Record[]} props.records
  * @param {function} props.onChange
+ * @param {function} props.onRemove
+ * @param {number} props.index
  */
-const AddAddressForm = ({ records, onChange }) => {
+const AddAddressForm = ({ records, onChange, onRemove, index }) => {
+  console.log("rerendering form", index);
+  const [key, setKey] = useState(null);
   const [keyError, setKeyError] = useState(null);
+  const [val, setVal] = useState(null);
   const [valError, setValError] = useState(null);
 
   const handleOnChangeKey = (key) => {
-    const keyIsDuplicated = records.some((r) => r.key === key)
+    const keyIsDuplicated = records.some((r) => r.key === key);
     setKeyError(keyIsDuplicated ? keyIsDuplicatedErrorMessage : null);
-    onChange('key', key);
-    onChange('isKeyValid', !keyIsDuplicated);
+    setKey(key);
+    onChange("key", key);
+    onChange("isKeyValid", !keyIsDuplicated);
   };
 
   const handleOnBlurKey = (key) => {
-    const validAddressRegex = /^0x[a-fA-F0-9]{40}$/
-    const isValidAddress = validAddressRegex.test(key)
+    const validAddressRegex = /^0x[a-fA-F0-9]{40}$/;
+    const isValidAddress = validAddressRegex.test(key);
     setKeyError(isValidAddress ? null : "Must be a valid address");
-    onChange('isKeyValid', isValidAddress);
+    onChange("isKeyValid", isValidAddress);
   };
 
   const handleOnChangeVal = (val) => {
-    const valIsDuplicated = records.some((r) => r.val === val)
+    const valIsDuplicated = records.some((r) => r.val === val);
     setValError(valIsDuplicated ? valIsDuplicatedErrorMessage : null);
-    onChange('val', val);
-    onChange('isValValid', !valIsDuplicated);
+    setVal(val);
+    onChange("val", val);
+    onChange("isValValid", !valIsDuplicated);
   };
 
   return (
-    <div style={{ marginBottom: "1.5em" }}>
-      <ErrorAlert errorMessage={keyError} />
-      <Input
-        addonBefore={"Address"}
-        onChange={(evt) => handleOnChangeKey(evt.target.value)}
-        onBlur={(evt)=> handleOnBlurKey(evt.target.value)}
-      />
-
-      <ErrorAlert errorMessage={valError} />
-      <Input
-        addonBefore={"Name"}
-        onChange={(evt) => handleOnChangeVal(evt.target.value)}
-      />
-    </div>
+    <Space style={{ marginBottom: "1.5em" }}>
+      <Space direction="vertical">
+        <ErrorAlert errorMessage={keyError} />
+        <Input
+          addonBefore={"Address"}
+          value={key}
+          onChange={(evt) => handleOnChangeKey(evt.target.value)}
+          onBlur={(evt) => handleOnBlurKey(evt.target.value)}
+        />
+        <ErrorAlert errorMessage={valError} />
+        <Input
+          value={val}
+          addonBefore={"Name"}
+          onChange={(evt) => handleOnChangeVal(evt.target.value)}
+        />
+      </Space>
+      {index > 0 ? (
+        <div>
+          <Button
+            type="ghost"
+            onClick={() => onRemove()}
+            icon={<MinusOutlined />}
+          ></Button>
+        </div>
+      ) : null}
+    </Space>
   );
 };
 
@@ -90,9 +113,9 @@ export const AddAddressesButton = ({
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
-    const keysAreValid = recordsToAdd.every(r => r.isKeyValid)
-    const valuesAreValid = recordsToAdd.every(r => r.isValValid)
-    const formIsValid = keysAreValid && valuesAreValid
+    const keysAreValid = recordsToAdd.every((r) => r.isKeyValid);
+    const valuesAreValid = recordsToAdd.every((r) => r.isValValid);
+    const formIsValid = keysAreValid && valuesAreValid;
     setIsFormValid(formIsValid);
   }, [records, recordsToAdd]);
 
@@ -102,14 +125,14 @@ export const AddAddressesButton = ({
     setError("");
     setIsFormValid(false);
     setIsLoading(false);
-  }
+  };
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
   const hideModal = () => {
-    resetState()
+    resetState();
   };
 
   const handleCancel = () => {
@@ -120,13 +143,23 @@ export const AddAddressesButton = ({
     addRecords();
   };
 
-  const handleOnChangeToRecordToAdd = (index) => (property, value) => 
+  const handleOnChange = (index) => (property, value) =>
     setRecordsToAdd((recordsToAdd) => {
       const _recordsToAdd = [...recordsToAdd];
       _recordsToAdd[index][property] = value;
       return _recordsToAdd;
-    })
-  
+    });
+
+  const handleOnRemove = (index) => () =>
+    setRecordsToAdd((recordsToAdd) => {
+      const newArr = [...recordsToAdd];
+      newArr.splice(index, 1);
+      console.log(
+        "🚀 ~ file: AddAddressesButton.js ~ line 157 ~ setRecordsToAdd ~ newArr",
+        newArr
+      );
+      return newArr;
+    });
 
   const addRecords = () => {
     setIsLoading(true);
@@ -141,7 +174,7 @@ export const AddAddressesButton = ({
       setIsLoading(false);
       if (err) return setError(err);
       addToRecordsInState(recordsToAdd);
-      resetState()
+      resetState();
     });
   };
 
@@ -157,24 +190,32 @@ export const AddAddressesButton = ({
       <Modal
         title="Add Address Tags"
         visible={isModalVisible}
+        maskClosable={false}
         onOk={handleAdd}
         onCancel={handleCancel}
         footer={[
           <Button type="link" onClick={handleCancel}>
             Cancel
           </Button>,
-          <Button type="primary" disabled={!isFormValid} loading={isLoading} onClick={handleAdd}>
+          <Button
+            type="primary"
+            disabled={!isFormValid}
+            loading={isLoading}
+            onClick={handleAdd}
+          >
             Add
           </Button>,
         ]}
       >
         <Space direction="vertical" style={{ width: "100%" }}>
           <ErrorAlert errorMessage={error} />
-          {recordsToAdd.map((_, i) => (
+          {recordsToAdd.map((record, i) => (
             <AddAddressForm
               records={records}
-              key={i}
-              onChange={handleOnChangeToRecordToAdd(i)}
+              key={record.id}
+              index={i}
+              onChange={handleOnChange(i)}
+              onRemove={handleOnRemove(i)}
             />
           ))}
           <Button
