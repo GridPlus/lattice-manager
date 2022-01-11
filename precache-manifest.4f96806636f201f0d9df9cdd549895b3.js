054af41ce783cdcf30ef5ddfa0cde68897356571
douglance <douglance@users.noreply.github.com>
2022-01-11 19:21:09 +0000

    deploy: 1d20120ed1cd1617d0a6edb2b091ae5205d735ae

---
+++ precache-manifest.4f96806636f201f0d9df9cdd549895b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b84bc13197bc5618fd3d37303954a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0257ef48f3d35e666ed",
+    "url": "/static/css/2.3392a180.chunk.css"
+  },
+  {
+    "revision": "cbd50e9967121a413343",
+    "url": "/static/css/main.e374bcb2.chunk.css"
+  },
+  {
+    "revision": "e0257ef48f3d35e666ed",
+    "url": "/static/js/2.34213a32.chunk.js"
+  },
+  {
+    "revision": "b0ed1d8a0cc1f44153a78475fc21a986",
+    "url": "/static/js/2.34213a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd50e9967121a413343",
+    "url": "/static/js/main.e827b6eb.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
