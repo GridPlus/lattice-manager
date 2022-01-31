339c1c34ca33ef21d153fa453bf3557144368007
Doug Lance <doug.lance@gmail.com>
2022-01-31 09:51:13 -0500

    Adds AddressTable

---
+++ src/components/AddressTable.tsx
@@ -0,0 +1,128 @@
+import { LoadingOutlined } from "@ant-design/icons";
+import { Button, Input, Table } from "antd";
+import fuzzysort from "fuzzysort";
+import intersectionBy from "lodash/intersectionBy";
+import React, { useCallback, useEffect, useState } from "react";
+import { Record } from "../types/records";
+import { constants } from "../util/helpers";
+import { abbreviateHash } from "../util/strings";
+const { ADDRESSES_PER_PAGE } = constants;
+
+/**
+ * `AddressTable` is a table of key-value pairs of names and hashes with some management features to
+ * make it easier to manage a large amount of addresses.
+ *
+ * @param `addresses` - the list of key-value records to display
+ * @param `isLoading` - the table displays a loading spinner when true
+ * @param `removeSelected` - callback that lets the parent component remove a selected record
+ */
+export const AddressTable = ({
+  addresses,
+  isLoading,
+  removeSelected,
+}: {
+  addresses: Record[];
+  isLoading: boolean;
+  removeSelected: (selectedAddresses: Record[]) => void;
+}) => {
+  const [input, setInput] = useState("");
+  const [filteredAddresses, setFilteredAddresses] = useState([]);
+  const [selectedAddresses, setSelectedAddresses] = useState([]);
+
+  useEffect(() => {
+    setInput("");
+    setFilteredAddresses(addresses);
+  }, [addresses, isLoading]);
+
+  const filter = useCallback(
+    (value) =>
+      fuzzysort
+        .go(value, addresses, { keys: ["key", "val"] })
+        .map((x) => x.obj),
+    [addresses]
+  );
+
+  const handleOnSelect = (_, __, _selectedAddresses) => {
+    setSelectedAddresses(_selectedAddresses);
+  };
+
+  const handleOnSelectAll = (_, _selectedAddresses) => {
+    setSelectedAddresses(_selectedAddresses);
+  };
+
+  const onChange = ({ target: { value } }) => {
+    setInput(value);
+    const _addresses = value ? filter(value) : addresses;
+    setFilteredAddresses(_addresses);
+    setSelectedAddresses(intersectionBy(selectedAddresses, _addresses, "key"));
+  };
+
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <Input
+          value={input}
+          placeholder="Filter"
+          disabled={isLoading}
+          onChange={onChange}
+          style={{ marginBottom: "1em" }}
+          allowClear
+        />
+        <Button
+          danger
+          type="text"
+          disabled={selectedAddresses.length === 0}
+          onClick={()=>removeSelected(selectedAddresses)}
+          style={{ marginLeft: "1em" }}
+        >
+          Remove Selected
+        </Button>
+      </div>
+      <Table
+        dataSource={filteredAddresses}
+        tableLayout="fixed"
+        loading={{
+          spinning: isLoading,
+          tip: "Loading...",
+          indicator: <LoadingOutlined />,
+        }}
+        pagination={{
+          position: ["bottomCenter"],
+          pageSize: ADDRESSES_PER_PAGE,
+          defaultCurrent: 1,
+          showSizeChanger: false,
+        }}
+        rowSelection={{
+          type: "checkbox",
+          onSelect: handleOnSelect,
+          onSelectAll: handleOnSelectAll,
+          selectedRowKeys: selectedAddresses.map((x) => x.key),
+        }}
+      >
+        <Table.Column
+          title="Name"
+          dataIndex="val"
+          key="val"
+          defaultSortOrder="ascend"
+          sorter={(a: any, b: any) => a.val.localeCompare(b.val)}
+        />
+        <Table.Column
+          title="Address"
+          dataIndex="key"
+          key="key"
+          render={(key) => (
+            <a
+              className="lattice-a"
+              href={`https://etherscan.io/address/${key}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {abbreviateHash(key)}
+            </a>
+          )}
+          sorter={(a: any, b: any) => a.key.localeCompare(b.key)}
+        />
+      </Table>
+    </div>
+  );
+};
---
+++ src/components/__tests__/AddressTable.test.tsx
@@ -0,0 +1,86 @@
+import { fireEvent, render, screen, within } from "@testing-library/react";
+import React from "react";
+import { AddressTable } from "../AddressTable";
+
+const addresses = [
+  { key: "a", val: "a" },
+  { key: "b", val: "b" },
+];
+const removeSelected = jest.fn();
+const loading = false;
+const mockAddressTable = (overwrites?) => (
+  <AddressTable
+    {...{
+      addresses,
+      loading,
+      removeSelected,
+      ...overwrites,
+    }}
+  />
+);
+
+describe("AddressTable", () => {
+  it("renders", () => {
+    render(mockAddressTable());
+  });
+
+  it("shows loading", () => {
+    render(mockAddressTable({ loading: true }));
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("selects and unselects addresses", () => {
+    render(mockAddressTable());
+    const checkboxes = screen.getAllByRole("checkbox");
+    const removeButton = screen.getByRole("button", {
+      name: "Remove Selected",
+    });
+    expect(removeButton).toBeDisabled();
+    const selectAll = checkboxes[0];
+    fireEvent.click(selectAll);
+    expect(selectAll).toBeChecked();
+    const selectA = checkboxes[1];
+    fireEvent.click(selectA);
+    expect(selectA).not.toBeChecked();
+  });
+
+  it("handles removing addresses", () => {
+    render(mockAddressTable());
+    const checkboxes = screen.getAllByRole("checkbox");
+    const removeButton = screen.getByRole("button", {
+      name: "Remove Selected",
+    });
+    expect(removeButton).toBeDisabled();
+    const selectAll = checkboxes[0];
+    fireEvent.click(selectAll);
+    expect(removeButton).not.toBeDisabled();
+    fireEvent.click(removeButton);
+    expect(removeSelected).toHaveBeenCalled();
+  });
+
+  it("filters addresses", () => {
+    render(mockAddressTable());
+    expect(screen.getByText("a")).toBeInTheDocument();
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "b" } });
+    expect(screen.queryByText("a")).not.toBeInTheDocument();
+    fireEvent.change(input, { target: { value: "" } });
+    expect(screen.queryByText("a")).toBeInTheDocument();
+  });
+
+  it("sorts addresses", () => {
+    render(mockAddressTable());
+    expect(screen.getByText("a")).toBeInTheDocument();
+    const sortByName = screen.getByText("Name");
+    const sortByAddress = screen.getByText("Address");
+    const rowOne = () => screen.queryAllByRole("row")[1];
+    fireEvent.click(sortByName);
+    expect(within(rowOne()).getByText("b")).toBeInTheDocument();
+    fireEvent.click(sortByName);
+    expect(within(rowOne()).getByText("a")).toBeInTheDocument();
+    fireEvent.click(sortByAddress);
+    expect(within(rowOne()).getByText("a")).toBeInTheDocument();
+    fireEvent.click(sortByAddress);
+    expect(within(rowOne()).getByText("b")).toBeInTheDocument();
+  });
+});
