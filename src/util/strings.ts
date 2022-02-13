16628e95d83238740a26208c8b558912bb20fb4c
Doug Lance <doug.lance@gmail.com>
2022-02-13 15:13:28 -0500

    Adds validity check to utils and renames utils/strings.ts

--- src/util/strings.ts
+++
@@ -1,9 +0,0 @@
-
-/**
- * `abbreviateHash` takes a string and returns the first 10 and last 8 characters with an ellipsis between.
- * @param {string} hash - the hash string to shorten.
- */
-export const abbreviateHash = (hash: string) => hash ?`${hash.slice(0, 10)}...${hash.slice(
-  hash.length - 8,
-  hash.length
-)}` : ""
