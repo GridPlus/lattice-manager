caba398987887d60a68dec0e7dc4963a1f746733
Doug Lance <doug.lance@gmail.com>
2022-03-07 13:59:13 -0500

    Improves useFeature API and adds tests

--- src/hooks/useFeature.tsx
+++ src/hooks/useFeature.tsx
@@ -3,11 +3,20 @@ import SDKSession from "../sdk/sdkSession";
 /**
  * `useFeature` is a React hook for feature flags that makes it easy to know when a particular
  * feature is active for a version of the Lattice firmware (or other external data).
+ * 
+ * To add a feature, add a SNAKE_CASE key to the `features` variable with a value of 
  */
-export const useFeature = ( session: SDKSession): { [key: string]: boolean } => {
+ export const useFeature = ( session: SDKSession): { [feature: string]: boolean } => {
   const { fix, minor, major } = session.client.getFwVersion();
 
-  return {
-    CAN_VIEW_CONTRACTS: minor >= 14,
+  const features = {
+    CAN_VIEW_CONTRACTS: [0, 14, 0],
   };
+
+  return Object.fromEntries(
+    Object.entries(features).map(([key, [_fix, _minor, _major]]) => [
+      key,
+      fix >= _fix && minor >= _minor && major >= _major,
+    ])
+  );
 };
