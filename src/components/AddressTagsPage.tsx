1b8e58d022e6f57446919ff25924bee24fea623a
Doug Lance <doug.lance@gmail.com>
2022-05-11 12:24:48 -0400

    Removes useEffect dep that caused looped fetching

--- src/components/AddressTagsPage.tsx
+++ src/components/AddressTagsPage.tsx
@@ -25,7 +25,7 @@ const AddressTagsPage = () => {
       fetchAddresses();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetchAddresses]);
+  }, []);
 
   const extra = [
     <Button
