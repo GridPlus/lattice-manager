79dbac2c15746fc3a75c1f6e9e058eeecc928ba1
Doug Lance <doug.lance@gmail.com>
2022-12-15 12:17:41 -0500

    Adds GridPunks, Explorer, newsletter to landing page

--- src/components/LandingCard.tsx
+++ src/components/LandingCard.tsx
@@ -20,7 +20,7 @@ export const LandingCard = ({
   <div style={{ flexGrow: 1, flexBasis: 0 }}>
     <a
       onClick={(event) => {
-        if (link === "settings") {
+        if (link === PAGE_KEYS.SETTINGS) {
           event.preventDefault();
           if (isBtcWalletActive()) {
             goToPage(PAGE_KEYS.WALLET);
@@ -28,9 +28,13 @@ export const LandingCard = ({
             goToPage(PAGE_KEYS.SETTINGS);
           }
         }
-        if (link === "address-tags") {
+        if (link === PAGE_KEYS.ADDRESS_TAGS) {
           event.preventDefault();
           goToPage(PAGE_KEYS.ADDRESS_TAGS);
+        }
+        if (link === PAGE_KEYS.EXPLORER) {
+          event.preventDefault();
+          goToPage(PAGE_KEYS.EXPLORER);
         }
       }}
       href={link}
@@ -42,6 +46,12 @@ export const LandingCard = ({
         style={{
           height: "100%",
           width: "100%",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
         hoverable
       >
--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -4,10 +4,14 @@ import {
   TwitterOutlined,
   YoutubeOutlined,
   InfoCircleOutlined,
+  WalletOutlined,
+  MailOutlined,
+  ReconciliationOutlined,
 } from "@ant-design/icons";
 import { faBtc } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Alert, Card } from "antd";
+import { Alert, Button, Card } from "antd";
+import { constants } from "../util/helpers";
 import React from "react";
 import { useFeature } from "../hooks/useFeature";
 import { PageContent } from "./index";
@@ -15,76 +19,113 @@ import { LandingCard } from "./LandingCard";
 
 const Landing = ({ goToPage }) => {
   const { USES_AUTO_ABI } = useFeature();
+  const PAGE_KEYS = constants.PAGE_KEYS;
 
   return (
     <PageContent>
-      <Card
-        style={{ textAlign: "center" }}
-        headStyle={{ fontSize: "32px" }}
-        title="Lattice Manager"
-      >
+      <Card style={{ textAlign: "center" }}
+              headStyle={{ fontSize: "32px" }}
+              title="Lattice Manager">
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            gap: "30px",
-            margin: "25px",
+            gap: "2em",
+            margin: "15px",
+            justifyItems: "center",
           }}
         >
           <div
             style={{
               display: "flex",
-              flexWrap: "wrap",
-              alignItems: "center",
-              justifyContent: "center",
+              flexDirection: "column",
+              gap: "1em",
             }}
           >
-            <div style={{ width: "200px" }}>
-              <p style={{ fontSize: "1.2em" }}>
-                Manage secure data on your Lattice hardware wallet device for a
-                better web3 experience
-              </p>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div style={{ width: "200px" }}>
+                <p style={{ fontSize: "1.2em" }}>
+                  Manage secure data on your Lattice hardware wallet device for
+                  a better web3 experience
+                </p>
+              </div>
+              <img
+                src="lattice-landing.png"
+                style={{ maxWidth: "250px" }}
+                alt="lattice-one-device"
+              />
             </div>
-            <img
-              src="lattice-landing.png"
-              style={{ maxWidth: "250px" }}
-              alt="lattice-one-device"
-            />
           </div>
           <div>
             <h2>Features</h2>
             <div
               style={{
-                display: "flex",
-                gap: "10px",
-                justifyContent: "center",
-                flexWrap: "wrap",
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+                gap: "1em",
               }}
             >
               <LandingCard
                 title="Address Tags"
                 body="Tag your favorite contracts or addresses"
                 icon={<TagsOutlined />}
-                link={"address-tags"}
+                link={PAGE_KEYS.ADDRESS_TAGS}
+                goToPage={goToPage}
+              />
+              <LandingCard
+                title="Wallet Explorer"
+                body="Find and tag addresses by derivation path"
+                icon={<WalletOutlined />}
+                link={PAGE_KEYS.EXPLORER}
                 goToPage={goToPage}
               />
               <LandingCard
                 title="Bitcoin Wallet"
                 body={"Check balances and send BTC transactions"}
                 icon={<FontAwesomeIcon icon={faBtc} />}
-                link="settings"
+                link={PAGE_KEYS.SETTINGS}
                 goToPage={goToPage}
               />
             </div>
           </div>
           <div>
+            <Card
+              bodyStyle={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                src="gridpunk.png"
+                style={{ maxWidth: "250px" }}
+                alt="lattice-one-device"
+              />
+              <div style={{ width: "200px" }}>
+                <h2>Mint your GridPunk</h2>
+                <p>
+                  GridPunk digital collectibles are a gift from GridPlus to
+                  Lattice1 owners.
+                </p>
+                <Button href="https://gridplus.io/gridpunks">Learn More</Button>
+              </div>
+            </Card>
+          </div>
+          <div>
             <h2>Resources</h2>
             <div
               style={{
-                display: "flex",
-                gap: "10px",
-                justifyContent: "center",
-                flexWrap: "wrap",
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+                gap: "1em",
               }}
             >
               <LandingCard
@@ -113,6 +154,20 @@ const Landing = ({ goToPage }) => {
                 body="Backup or create new wallets"
                 icon={<CreditCardOutlined />}
                 link="https://gridplus.io/safecards"
+                goToPage={goToPage}
+              />
+              <LandingCard
+                title="Newsletter"
+                body="Subscribe for the latest from GridPlus"
+                icon={<MailOutlined />}
+                link="https://gridplus.io/subscribe"
+                goToPage={goToPage}
+              />
+              <LandingCard
+                title="Survey"
+                body="Help us improve your experience"
+                icon={<ReconciliationOutlined />}
+                link="https://gridplus.io/survey"
                 goToPage={goToPage}
               />
             </div>
