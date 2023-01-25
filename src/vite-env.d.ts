150eee29f1592d4ae5cee658f286787d99aa0431
Doug Lance <doug.lance@gmail.com>
2023-01-25 14:34:01 -0500

    Removes BTC_COIN environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 declare const APP_VERSION: string;
 declare const ENV: string;
 declare const BASE_SIGNING_URL: string;
-declare const BTC_COIN: string;
 declare const BTC_DEFAULT_FEE_RATE: string;
 declare const BTC_TX_BASE_URL: string;
 declare const BTC_TESTNET: string;
